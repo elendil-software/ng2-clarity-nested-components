3d55d25b9e0a59320a8451de6a04ca78685ca757
Julien Tschäppät <git@elendil.software>
2017-02-09 19:15:02 +0100

    correction of the subnav-sidebar case

--- src/app/subnav/subnav-main/subnav-main.component.ts
+++ src/app/subnav/subnav-main/subnav-main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-subnav-main',
+  host: { '[class.main-container]': 'true' },
   templateUrl: './subnav-main.component.html',
   styleUrls: ['./subnav-main.component.css']
 })
